ba21c72e6b39081bf737ada0da4bd2b87c8aec2a
Luen <dudrhsdl15@naver.com>
2021-09-15 08:33:40 +0900

    commit

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    return HttpResponse("집가고싶다 집가고싶다 집가고싶다 집가고싶다 집가고싶다 집가고싶다 집가고싶다 집가고싶다\n집가고싶다 집가고싶다 집가고싶다 집가고싶다 집가고싶다 집가고싶다 집가고싶다 집가고싶다 ")
 
 # Create your views here.
